bd07b288c3560ed20ff6aafa82b3b977e3287803
SeiyaCooper <120044030+SeiyaCooper@users.noreply.github.com>
2025-07-21 17:24:41 +0800

    feat: use the tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,30 +9,83 @@ pub fn run() {
                         .level(log::LevelFilter::Info)
                         .build(),
                 )?;
-                app.handle()
-                    .plugin(tauri_plugin_store::Builder::default().build())?;
-                #[cfg(desktop)]
-                {
-                    use tauri::{Emitter, Manager};
-                    use tauri_plugin_clipboard_manager::ClipboardExt;
-                    use tauri_plugin_global_shortcut::ShortcutState;
-                    app.handle().plugin(
-                        tauri_plugin_global_shortcut::Builder::new()
-                            .with_shortcut("CommandOrControl+Shift+C")?
-                            .with_handler(|app, _shortcut, event| {
-                                if event.state == ShortcutState::Pressed {
-                                    if let Ok(text) = app.clipboard().read_text() {
-                                        let window = app.get_webview_window("main").unwrap();
-                                        window.unminimize().unwrap();
-                                        window.set_focus().unwrap();
-                                        app.emit("window-unminimize", text).unwrap();
-                                    }
-                                }
-                            })
-                            .build(),
-                    )?;
-                }
             }
+
+            app.handle()
+                .plugin(tauri_plugin_store::Builder::default().build())?;
+
+            #[cfg(desktop)]
+            {
+                use tauri::menu::{Menu, MenuItem};
+                use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+                use tauri::{AppHandle, Emitter, Manager, WindowEvent};
+                use tauri_plugin_clipboard_manager::ClipboardExt;
+                use tauri_plugin_global_shortcut::ShortcutState;
+
+                let window = app.get_webview_window("main").unwrap();
+                let window_clone = window.clone();
+                window.on_window_event(move |event| match event {
+                    WindowEvent::CloseRequested { api, .. } => {
+                        api.prevent_close();
+                        let _ = window_clone.hide();
+                        let _ = window_clone.set_skip_taskbar(true);
+                    }
+                    _ => {}
+                });
+
+                fn display_main_window(app: &AppHandle) {
+                    if let Some(window) = app.get_webview_window("main") {
+                        let _ = window.set_skip_taskbar(false);
+                        let _ = window.show();
+                        let _ = window.unminimize();
+                        let _ = window.set_focus();
+                    }
+                }
+
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new()
+                        .with_shortcut("CommandOrControl+Shift+C")?
+                        .with_handler(|app, _shortcut, event| {
+                            if event.state == ShortcutState::Pressed {
+                                if let Ok(text) = app.clipboard().read_text() {
+                                    display_main_window(app);
+                                    app.emit("window-unminimize", text).unwrap();
+                                }
+                            }
+                        })
+                        .build(),
+                )?;
+
+                let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+                let menu = Menu::with_items(app, &[&quit_item])?;
+
+                TrayIconBuilder::new()
+                    .menu(&menu)
+                    .show_menu_on_left_click(false)
+                    .on_menu_event(|app, event| match event.id.as_ref() {
+                        "quit" => {
+                            app.exit(0);
+                        }
+                        _ => {}
+                    })
+                    .on_tray_icon_event(|tray, event| match event {
+                        TrayIconEvent::Click {
+                            button: MouseButton::Left,
+                            button_state: MouseButtonState::Up,
+                            ..
+                        } => {
+                            let app = tray.app_handle();
+
+                            if let Some(_) = app.get_webview_window("main") {
+                                display_main_window(app);
+                            }
+                        }
+                        _ => {}
+                    })
+                    .icon(app.default_window_icon().unwrap().clone())
+                    .build(app)?;
+            }
+
             Ok(())
         })
         .run(tauri::generate_context!())
