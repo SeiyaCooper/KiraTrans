4d3d7d9f85a6b2fa6238032773939cac8978b715
SeiyaCooper <120044030+SeiyaCooper@users.noreply.github.com>
2025-07-31 21:05:51 +0800

    feat: add splash screen for android

--- src-tauri/gen/android/app/src/main/java/com/seiyacooper/kiratrans/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/seiyacooper/kiratrans/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : TauriActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
